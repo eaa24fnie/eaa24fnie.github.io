4c54c325577749b1d7ef3582e4ceb0a72f1b8c1f
Frederik Nielsen <eaa24fnie@students.eaaa.dk>
2025-09-19 10:22:00 +0200

    Grain again

--- src/App.jsx
+++ src/App.jsx
@@ -16,17 +16,26 @@ export default function App() {
             numOctaves="2"
             seed="2"
           >
+            {/* Animate baseFrequency to simulate moving noise */}
             <animate
-              attributeName="seed"
-              from="0"
-              to="100"
-              dur="10s"
+              attributeName="baseFrequency"
+              values="0.65;0.75;0.7;0.72;0.68;0.7"
+              dur="5s"
               repeatCount="indefinite"
             />
           </feTurbulence>
-          <feDisplacementMap in="SourceGraphic" scale="20" />
+          <feDisplacementMap in="SourceGraphic" scale="20">
+            {/* Slightly animate scale to add extra jitter */}
+            <animate
+              attributeName="scale"
+              values="18;22;20;21;19;20"
+              dur="5s"
+              repeatCount="indefinite"
+            />
+          </feDisplacementMap>
         </filter>
       </svg>
+
       <main className="main">
         <Routes>
           <Route path="/" element={<Home />} />
